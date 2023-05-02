490f0e808a0b31f9a65cff9b389c4bd4a64a94dc
nelitonps <neliton.net@hotmail.com>
2023-05-02 09:22:00 -0300

    BorderException and defensive programming

---
+++ src/boardgame/BoardException.java
@@ -0,0 +1,14 @@
+package boardgame;
+
+//RuntimeException - é uma exceção opcional de ser tratada
+public class BoardException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public BoardException(String msg) {
+		super(msg);
+	}
+	
+	
+
+}
